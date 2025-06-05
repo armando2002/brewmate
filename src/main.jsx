b5bcdf0d52b569a54db5b785d37e67fd2fda7afd
Stephen <armando2002@gmail.com>
2025-06-05 10:38:47 -0700

    updated routes

--- src/main.jsx
+++ src/main.jsx
@@ -11,9 +11,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
+  <Route path="*" element={<App />} />
+  <Route path="/home" element={<Home />} />
+</Routes>
     </Router>
   </React.StrictMode>
 );
